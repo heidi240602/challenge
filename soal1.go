9ae7510f852b80fad54030c3847dedb2ff9c8601
Heidi Renata Halim <heidi.renata@ui.ac.id>
2024-03-25 12:10:31 +0700

    add more explanation

--- soal1.go
+++ soal1.go
@@ -18,28 +18,30 @@ func (tim Tim) rerata() float64 {
 	return rerata
 }
 
-//fungsi untuk membandingkan 2 skor tanpa ada persyaratan minimum
+//fungsi dasar untuk membandingkan 2 skor rata-rata saja tanpa ada persyaratan minimum
 func (tim1 Tim) adu1(tim2 Tim) {
 	var res1 = tim1.rerata()
 	var res2 = tim2.rerata()
 
-	if res1 > res2 { //jika yang lebih besar kelompok 1
+	fmt.Printf("Skor tim %s adalah %3.f dan skor tim %s adalah %3.f \n", tim1.nama, res1, tim2.nama, res2)
+	if res1 > res2 { //jika nilai rerata yang lebih besar kelompok 1
 		fmt.Printf("yang menang adalah tim %s dengan skor %.3f\n", tim1.nama, res1)
-	} else if res2 > res1 { //jika yang lebih besar kelompok 2
+	} else if res2 > res1 { //jika nilai rerata yang lebih besar kelompok 2
 		fmt.Printf("yang menang adalah tim %s dengan skor %.3f\n", tim2.nama, res2)
-	} else { //jika seri
+	} else { //jika nilai sama besarnya atau seri
 		fmt.Println("seri")
 	}
 }
 
 //untuk yang memakai prasyarat harus minimal >=100
 func (tim1 Tim) adu2(tim2 Tim) {
-	var res1 = tim1.rerata()
-	var res2 = tim2.rerata()
+	var res1 = tim1.rerata() //menghitung rata2 nilai dari tim kesatu
+	var res2 = tim2.rerata() //menghitung rerata nilai dri tim kedua
 
-	if (res1 > res2) && (res1 >= 100) {
+	fmt.Printf("Skor tim %s adalah %3.f dan skor tim %s adalah %3.f \n", tim1.nama, res1, tim2.nama, res2)
+	if (res1 > res2) && (res1 >= 100) { //jika yang menang adalah tim kesatu
 		fmt.Printf("yang menang adalah tim %s dengan skor %.3f\n", tim1.nama, res1)
-	} else if (res2 > res1) && (res2 >= 100) {
+	} else if (res2 > res1) && (res2 >= 100) { //jika yang lbh besar nilai reratanya adalah tim kedua
 		fmt.Printf("yang menang adalah tim %s dengan skor %.3f\n", tim2.nama, res2)
 	} else if (res2 == res1) && (res1 >= 100) { //seri kalau keduanya sama dan diatas 100 semua
 		fmt.Println("seri")
@@ -52,8 +54,8 @@ func main() {
 	skorl := [3]int{96, 108, 89}
 	dilumba := Tim{"Lumba-lumba", skorl}
 	skork := [3]int{88, 91, 110}
-	dikoala := Tim{"Lumba-lumba", skork}
-	dilumba.adu1(dikoala)
+	dikoala := Tim{"Koala", skork}
+	dilumba.adu1(dikoala) //cara mencari tahu yang lebih besarnya
 	//soal2
 	skorl2 := [3]int{97, 112, 101}
 	skork2 := [3]int{109, 95, 123}
--- soal2.go
+++ soal2.go
@@ -3,20 +3,20 @@ package main
 import "fmt"
 
 type BMI struct { //struct untuk meringkas data bmi milik orang
-	nama          string
-	berat, tinggi float64
+	nama          string  //string nama pemilik BMI
+	berat, tinggi float64 //berat dan tinggi yang didokumentasi
 }
 
 func (bmi BMI) countBMI() float64 { //fungsi untuk menghitung BMI
-	massa := bmi.berat
-	tinggi := bmi.tinggi
-	res := massa / (tinggi * tinggi)
+	massa := bmi.berat               //mengambil besar massa dari objek bmi dan memasukannya ke variabel masa
+	tinggi := bmi.tinggi             //mengambil besar tinggi nya dan dimasukkan ke variabel tinggi
+	res := massa / (tinggi * tinggi) //menerapkan rumus bmi
 	return res
 }
 func (bmi1 BMI) isLarger(bmi2 BMI) bool {
-	res1 := bmi1.countBMI()
-	res2 := bmi2.countBMI()
-	if res1 > res2 { //cek apakah bmi kesatu lebih besar dari bmi kedua, klo iya balikin true, klo gk BALIKIN false
+	res1 := bmi1.countBMI() //menghitung BMI dari objek pertama
+	res2 := bmi2.countBMI() //hitung BMI objek kedua
+	if res1 > res2 {        //cek apakah bmi kesatu lebih besar dari bmi kedua, klo iya balikin true, klo gk BALIKIN false
 		return true
 	} else {
 		return false
@@ -25,7 +25,7 @@ func (bmi1 BMI) isLarger(bmi2 BMI) bool {
 func main() {
 	mark := BMI{"Mark", 78, 1.69}
 	john := BMI{"John", 92, 1.95}
-	markHigherBMI := mark.isLarger(john)
+	markHigherBMI := mark.isLarger(john) //mencari tahu apakah BMI mark benar lebih besar dari john dan melempar hasilnya ke variabel ini
 	fmt.Println(markHigherBMI)
 	//soal nomor 2
 	mark.berat = 95
